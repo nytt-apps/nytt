8c02e512480cec243e138e2bf6838dc54ff3fcbf
abehidek <hidek.abe@outlook.com>
2022-05-31 11:25:36 -0300

    Switch to typescript and add basic rust backend

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -3,8 +3,25 @@
   windows_subsystem = "windows"
 )]
 
+#[tauri::command]
+fn say_hi(valor1: String) -> String {
+  println!("{}", valor1);
+  "Hello World!!".into()
+}
+
+// #[tauri::command]
+// fn test() -> String {
+//   let mut foo = String::new();
+//   foo.push('a');
+//   foo.push('r');
+//   foo.push('r');
+
+//   foo.into()
+// }
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![say_hi])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
---
+++ app/src/App.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import logo from './logo.svg';
+import Counter from './components/Counter'
+import './App.css';
+
+import { invoke } from '@tauri-apps/api/tauri'
+
+import { emit, listen } from '@tauri-apps/api/event'
+
+function App() {
+  fetch('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=d2460721257142e6ab02f85d003e882d')
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+  const handleClick = async () => {
+    console.log("js")
+    let bar = await invoke('say_hi', {
+      valor1: 'abe'
+    })
+    console.log(bar)
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx!</code> and save to reload.
+        </p>
+        <Counter />
+        <button onClick={handleClick} >Clique!</button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React!
+        </a>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
