018567349019cf9f1603f08e823fb900c4ea6d47
abehidek <hidek.abe@outlook.com>
2022-06-06 18:20:12 -0300

    Add nytt theme colors and logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "nytt": "#00154F",
+        "ttyy": "#3E3F81",
+        "yytt": "#F4AF1B",
+        "transparent": "transparent",
+      },
+    },
   },
   plugins: [],
 }
